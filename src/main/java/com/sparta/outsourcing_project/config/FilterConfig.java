a8561b8126f7819b9a17b6493a67ab76e3b5f6b0
areum0116 <areum0116@naver.com>
2024-09-20 03:14:24 +0900

    [feat] Create JwtFilter, JwtUtil, FilterConfig

--- src/main/java/com/sparta/outsourcing_project/config/FilterConfig.java
+++ src/main/java/com/sparta/outsourcing_project/config/FilterConfig.java
@@ -1,10 +1,23 @@
 package com.sparta.outsourcing_project.config;
 
+import com.sparta.outsourcing_project.config.jwt.JwtFilter;
+import com.sparta.outsourcing_project.config.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class FilterConfig {
+    private final JwtUtil jwtUtil;
 
+    @Bean
+    public FilterRegistrationBean<JwtFilter> jwtFilter() {
+        FilterRegistrationBean<JwtFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new JwtFilter(jwtUtil));
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
 }
--- src/main/java/com/sparta/outsourcing_project/domain/exception/AllExceptionHandler.java
+++ src/main/java/com/sparta/outsourcing_project/domain/exception/AllExceptionHandler.java
@@ -1,12 +1,10 @@
 package com.sparta.outsourcing_project.domain.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -16,16 +14,28 @@ import java.util.Map;
 @RestControllerAdvice
 public class AllExceptionHandler {
 
-    @ExceptionHandler(CannotFindOrderId.class)
-    public ResponseEntity<Map<String, Object>> CannotFindOrderId(CannotFindOrderId ex) {
+    private ResponseEntity<Map<String, Object>> buildResponse(String message, HttpStatus status) {
         Map<String, Object> responseBody = new HashMap<>();
-        responseBody.put("message", ex.getMessage());
-        responseBody.put("status", HttpStatus.NOT_FOUND.value());
+        responseBody.put("message", message);
+        responseBody.put("status", status.value());
         responseBody.put("timestamp", LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseBody);
+        return ResponseEntity.status(status).body(responseBody);
     }
 
+    @ExceptionHandler(CannotFindOrderId.class)
+    public ResponseEntity<Map<String, Object>> cannotFindOrderId(CannotFindOrderId ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
 
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<Map<String, Object>> authException(AuthException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, Object>> tokenException(JwtException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
 
 
     @ExceptionHandler(Exception.class)
