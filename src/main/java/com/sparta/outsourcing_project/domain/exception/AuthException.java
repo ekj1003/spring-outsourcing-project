bc8d736e335388c2db13eb043f9459ffd57e6596
areum0116 <areum0116@naver.com>
2024-09-19 22:26:35 +0900

    [feat] Create AuthUser, AuthException, WebConfig

---
+++ src/main/java/com/sparta/outsourcing_project/domain/exception/AuthException.java
@@ -0,0 +1,7 @@
+package com.sparta.outsourcing_project.domain.exception;
+
+public class AuthException extends RuntimeException{
+    public AuthException(String message) {
+        super(message);
+    }
+}
