1adf887a1e141aad4d35ba531b86a76e2f9c7028
Eunkyo <jeongeunkyo3@gmail.com>
2024-09-22 00:49:11 +0900

    [feat] allow customers to delete favorite stores

--- src/main/java/com/sparta/outsourcing_project/domain/store/repository/FavoriteStoreRepository.java
+++ src/main/java/com/sparta/outsourcing_project/domain/store/repository/FavoriteStoreRepository.java
@@ -5,6 +5,10 @@ import com.sparta.outsourcing_project.domain.store.entity.Store;
 import com.sparta.outsourcing_project.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FavoriteStoreRepository extends JpaRepository<FavoriteStore, Long> {
     boolean existsByUserAndStore(User user, Store store);
+
+    Optional<FavoriteStore> findByUserAndStore(User user, Store store);
 }
