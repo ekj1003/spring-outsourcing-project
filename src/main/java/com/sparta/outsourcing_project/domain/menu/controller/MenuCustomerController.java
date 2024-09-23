a27a907740c8536b7559f3ec1593131fe20d62b1
kiw <esfne4727@naver.com>
2024-09-23 17:12:34 +0900

    [feat] Added integrated search and search by menu type

--- src/main/java/com/sparta/outsourcing_project/domain/menu/controller/MenuCustomerController.java
+++ src/main/java/com/sparta/outsourcing_project/domain/menu/controller/MenuCustomerController.java
@@ -2,27 +2,33 @@ package com.sparta.outsourcing_project.domain.menu.controller;
 
 import com.sparta.outsourcing_project.config.authUser.Auth;
 import com.sparta.outsourcing_project.config.authUser.AuthUser;
+import com.sparta.outsourcing_project.domain.menu.dto.response.MenuListResponseDto;
 import com.sparta.outsourcing_project.domain.menu.dto.response.MenuResponse;
 import com.sparta.outsourcing_project.domain.menu.service.MenuCustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/customers/stores/{storeId}/menus")
+@RequestMapping("/customers/stores")
 @RequiredArgsConstructor
 public class MenuCustomerController {
     private final MenuCustomerService menuCustomerService;
 
     // Retrieve Store with Menus
-    @GetMapping
+    @GetMapping("/{storeId}/menus")
     public ResponseEntity<List<MenuResponse>> getStoreWithMenus(@PathVariable Long storeId, @Auth AuthUser authUser) {
         List<MenuResponse> menus = menuCustomerService.getStoreWithMenus(storeId, authUser.getId());
         return ResponseEntity.ok(menus);
+    }
+
+    // 메뉴를 타입별로 조회
+    @GetMapping("/{storeId}/menus/types")
+    public ResponseEntity<List<MenuListResponseDto>> getMenusByType(
+            @PathVariable Long storeId,
+            @RequestParam("menuType") String menuType) {
+        return ResponseEntity.ok(menuCustomerService.getMenusByType(storeId, menuType));
     }
 }
